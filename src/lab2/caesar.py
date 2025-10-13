f864e84f6b80d84ee5e81ebf1901cae72cdad305
quris-wa <wirrru@gmail.com>
2025-10-14 00:45:28 +0300

    Реализована функция encrypt_caesar()

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -11,7 +11,13 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for ch in plaintext:
+        if "A" <= ch <= "Z":
+            ciphertext += chr((ord(ch) - ord("A") + shift) % 26 + ord("A"))
+        elif "a" <= ch <= "z":
+            ciphertext += chr((ord(ch) - ord("a") + shift) % 26 + ord("a"))
+        else:
+            ciphertext += ch
     return ciphertext
 
 
