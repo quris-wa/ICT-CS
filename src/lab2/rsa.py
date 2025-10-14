521edf078fab3eb487fb14e35ea678e6dbfb394b
quris-wa <wirrru@gmail.com>
2025-10-14 11:03:31 +0300

    Реализована функция поиска НОД

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -31,8 +31,13 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    
+    return a + b
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
@@ -52,10 +57,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
+    n = p * q
     # PUT YOUR CODE HERE
 
-    # phi = (p-1)(q-1)
+    phi = (p - 1) * (q - 1)
     # PUT YOUR CODE HERE
 
     # Choose an integer e such that e and phi(n) are coprime
